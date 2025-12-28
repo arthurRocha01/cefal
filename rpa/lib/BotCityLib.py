11d21fe71bf5aa12ecf7df43f2cbcab4713b8728
Arthur Rocha <arthurrocha007.net@gmail.com>
2025-12-28 13:53:17 -0300

    feat: atualiza funções

--- rpa/lib/BotCityLib.py
+++ rpa/lib/BotCityLib.py
@@ -1,18 +1,22 @@
 from botcity.core import DesktopBot
-from utils import slows_flow
+from .ImageFileLoader import ImageFileLoader
+from ..utils import slows_flow
+import os
 
 class BotCityLib:
     def __init__(self, template, matching=0.95):
         self.template = template
         self.desktop = DesktopBot()
         self.matching = matching
+        self.base_dir = os.getcwd()
 
-    def _get_corresponding_image(self, field):
-        """Retorna o caminho da imagem correspondente ao campo."""
-        return f'resources/{self.template}/executions/{field}.png'
+        self.image_loader = ImageFileLoader(template)
+        self.images = self.image_loader.upload_images_files(self)
+        print(f'Mapa de imagens do Bot: {self.desktop.state.map_images.keys()}')
 
-    def _click_in_field(self, template, clicks=1):
+    def click_in_field(self, template, clicks=1):
         """Clica na imagem especificada pelo template."""
+        print(f'Buscando por: {template}')
         if not self.desktop.find(template, matching=self.matching):
             raise Exception(f'Imagem não encontrada: {template}')
         
@@ -21,5 +25,5 @@ class BotCityLib:
 
     def write(self, field, text):
         """Escreve o texto no campo identificado pela imagem template."""
-        self._click_in_field(self._get_corresponding_image(field))
+        self._click_in_field(self._get_corresponding_image(field), type)
         self.desktop.type_keys(text)
