a87ca75f4bd4aee524d25993da8736b3e060666b
Arthur Rocha <arthurrocha007.net@gmail.com>
2025-12-28 10:09:07 -0300

    feat: refatora camadas de RPA

--- rpa/handlers/HandlerRegister.py
+++ rpa/handlers/HandlerRegister.py
@@ -1,29 +1,9 @@
-from botcity.core import DesktopBot
-from utils import slows_flow
+from lib.BotCityLib import BotCityLib
 
 class HandlerRegister:
-  def __init__(self, template, matching=0.95):
-    self.template = template
-    self.desktop = DesktopBot()
-    self.matching = matching
-
-  def _get_corresponding_image(self, field):
-    """Retorna o caminho da imagem correspondente ao campo."""
-    return f'resources/{self.template}/executions/{field}.png'
-
-  def _click_in_field(self, template, clicks=1):
-    """Clica na imagem especificada pelo template."""
-    if not self.desktop.find(template, matching=self.matching):
-      raise Exception(f'Imagem não encontrada: {template}')
-    
-    self.desktop.click()
-    slows_flow()
-
-  def write(self, field, text):
-    """Escreve o texto no campo identificado pela imagem template."""
-    self._click_in_field(self._get_corresponding_image(field))
-    self.desktop.type_keys(text)
+  def __init__(self):
+    self.bot = BotCityLib('register_template')
 
   def save(self):
-    """Salva o produto clicando no botão de salvar."""
-    self._click_in_field('save_button.png')
+    """ Determina as operações para salvar o registro. """
+    pass
--- rpa/handlers/HandlerSteps.py
+++ rpa/handlers/HandlerSteps.py
@@ -1,3 +1,10 @@
+from lib.BotCityLib import BotCityLib
+
 class HandlerSteps:
-    def __init__(self):
+    def __init__(self, template):
+        self.template = template
+        self.bot = BotCityLib(template)
+
+    def take_steps(self):
+        """Executa os passos necessários para o registro."""
         pass
