3462d982dbfa5055bb0aa29e79e0fffa4a51b9cc
Arthur Rocha <arthurrocha007.net@gmail.com>
2025-12-28 13:52:24 -0300

    feat: Orchestrator inicial

--- pipelines/Orchestrator.py
+++ pipelines/Orchestrator.py
@@ -1,10 +1,12 @@
-from interface.ProcessInterface import ProcessInterface
-from rpa.handlers.HandlerRegister import HandlerRegister
+# from interface.ProcessInterface import ProcessInterface
+# from rpa.handlers.HandlerRegister import HandlerRegister
+from rpa.handlers.HandlerSteps import HandlerSteps
 
 class Orchestrator:
     def __init__(self):
-        self.process_interface = ProcessInterface()
-        self.register_handler = HandlerRegister()
+        # self.process_interface = ProcessInterface()
+        # self.register_handler = HandlerRegister()
+        self.steps_handler = HandlerSteps('register')
 
     def start_process(self):
-        pass
+        self.steps_handler.take_steps()
