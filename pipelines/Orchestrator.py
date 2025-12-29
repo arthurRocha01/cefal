8fc440de7cfe02a3966346ddb1bee09dcdb2e8fe
Arthur Rocha <arthurrocha007.net@gmail.com>
2025-12-29 11:34:03 -0300

    feat: integra Interface ao Orchestrator

--- pipelines/Orchestrator.py
+++ pipelines/Orchestrator.py
@@ -1,12 +1,13 @@
-# from interface.ProcessInterface import ProcessInterface
-# from rpa.handlers.HandlerRegister import HandlerRegister
-from rpa.handlers.HandlerSteps import HandlerSteps
+from ..interface.ProcessInterface import ProcessInterface
+from rpa.handlers.StepHandler import StepHandler
 
 class Orchestrator:
-    def __init__(self):
-        # self.process_interface = ProcessInterface()
-        # self.register_handler = HandlerRegister()
-        self.steps_handler = HandlerSteps('register')
+    def __init__(self, data_file_path):
+        self.data_file_path = data_file_path
+        self.proccess_interface = ProcessInterface()
+        self.steps_handler = StepHandler('register')
 
     def start_process(self):
-        self.steps_handler.take_steps()
+        """Inicia o processo de automação."""
+        data = self.proccess_interface.read_csv(self.data_file_path)
+        pass
