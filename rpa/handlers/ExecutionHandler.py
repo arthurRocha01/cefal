f2080a78dbf6130f172b0791b6ecf5ea2c32f535
Arthur Rocha <arthurrocha007.net@gmail.com>
2025-12-29 10:50:12 -0300

    chore: melhora semantica dos nomes das classes handlers

---
+++ rpa/handlers/ExecutionHandler.py
@@ -0,0 +1,20 @@
+from lib.BotCityLib import BotCityLib
+import re
+
+class HandlerRegister:
+  def __init__(self):
+    self.bot = BotCityLib('register')
+
+  def _sort_steps(self, steps):
+    return sorted(steps, key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x.stem)])
+    
+  def _get_steps_path(self):
+    steps = list(self.path.glob('*.png'))
+    return self._sort_steps(steps)
+
+  def save(self):
+    """ Determina as operações para salvar o registro. """
+    for step in self._get_steps_path():
+      step_name = step.stem
+      print(f'Executando passo: {step_name}')
+      self.bot.write(step_name, 'text')
--- rpa/handlers/HandlerRegister.py
+++
@@ -1,9 +0,0 @@
-from lib.BotCityLib import BotCityLib
-
-class HandlerRegister:
-  def __init__(self):
-    self.bot = BotCityLib('register_template')
-
-  def save(self):
-    """ Determina as operações para salvar o registro. """
-    pass
