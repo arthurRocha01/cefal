416b1817ef7cb402f9515fc86097cd4a51997fc8
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-07 00:11:09 -0300

    feat: adiciona suporte ao Orchestrator à nova arquitetura

--- pipelines/Orchestrator.py
+++ pipelines/Orchestrator.py
@@ -1,14 +1,15 @@
 from interface.ProcessInterface import ProcessInterface
-from rpa.Register import Register
+from rpa.flows import take_initial_steps
+from rpa.flows import register_product
 
 class Orchestrator:
     def __init__(self, data_file_path):
         self.data_file_path = data_file_path
         self.proccess_interface = ProcessInterface()
-        self.register = Register()
 
     def start_process(self):
         """Inicia o processo de automação."""
         data = self.proccess_interface.read_csv(self.data_file_path)
         print(f'Dados lidos: {data[0]}')
-        self.register.run(data)
+        # take_initial_steps.take_initial_steps()
+        register_product.register_products(data)
