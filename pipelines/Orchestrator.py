d3ba696768d6e9aa9a18e38c71bf23a2886e3779
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-07 21:33:24 -0300

    feat: torna caminho da entrada de dados fixa

--- pipelines/Orchestrator.py
+++ pipelines/Orchestrator.py
@@ -2,14 +2,19 @@ from interface.ProcessInterface import ProcessInterface
 from rpa.flows import take_initial_steps
 from rpa.flows import register_product
 
+from pathlib import Path
+
 class Orchestrator:
-    def __init__(self, data_file_path):
-        self.data_file_path = data_file_path
+    def __init__(self):
+        self.data_file_path = next(Path('resources/data').iterdir())
         self.proccess_interface = ProcessInterface()
 
     def start_process(self):
         """Inicia o processo de automação."""
         data = self.proccess_interface.read_csv(self.data_file_path)
         print(f'Dados lidos: {data[0]}')
-        # take_initial_steps.take_initial_steps()
+        try:
+            take_initial_steps.take_initial_steps()
+        except Exception:
+            print('Sem passos iniciais para executar.')
         register_product.register_products(data)
